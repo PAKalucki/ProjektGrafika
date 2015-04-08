661edbaad05276a546cd0f3affc0acf824c1ddfa
kidneyforfreenickname <jelitospudziszin@gmail.com>
2015-04-08 15:18:58 +0200

    Create szescian.h

---
+++ szescian.h
@@ -0,0 +1,7 @@
+#include <windows.h>   // standardowe pliki naglowkowe dla Windows 
+#include <glew.h>
+#include <glut.h>
+#include <GL/gl.h>     // interfejs OpenGL 
+#include <GL/glu.h>    // biblioteka dodatkowych narzedzi dla OpenGL  
+//#include <GL/glaux.h>  // biblioteka wspierajaca OpenGL
+#include <math.h>	   // biblioteka matematyczna
